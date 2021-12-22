ca2b79ad609bfb5e8b9eede09c66638a272d819a
Björn Andersson <bjorn.aaa@gmail.com>
2021-12-22 10:20:58 +0100

    Day 22

--- 21.py
+++ 21.py
@@ -1,10 +1,11 @@
-lines = [line[:-1] for line in open("21.txt", "r").readlines()]
+import heapq as hq
+lines = [line[:-1] for line in open("21test.txt", "r").readlines()]
 
 pos1 = int(lines[0][-1])
 pos2 = int(lines[1][-1])
 
 maxScore = 21
-nTimesReached = {(True, pos1, pos2, 0, 0) : 1}
+nTimesReached = {(0, True, pos1, pos2, 0, 0) : 1}
 freq = [(3,1), (4,3), (5,6), (6,7), (7,6), (8,3), (9,1)]
 def addToDict(k, v, d):
     d[k] = d[k] + v if k in d else v
@@ -15,40 +16,36 @@ def sim(p1Turn, p1, p2, score1, score2, n):
         if p1Turn:
             p1New = (p1 + dieSum) % 10
             score1New = score1 + (p1New if p1New > 0 else 10)
-            addToDict((not p1Turn, p1New, p2, score1New, score2), n * f, nTimesReached)
-            states.add((not p1Turn, p1New, p2, score1New, score2))
+            addToDict((score1New + score2, not p1Turn, p1New, p2, score1New, score2), n * f, nTimesReached)
+            states.add((score1New + score2,not p1Turn, p1New, p2, score1New, score2))
         else:
             p2New = (p2 + dieSum) % 10
             score2New = score2 + (p2New if p2New > 0 else 10)
-            addToDict((not p1Turn, p1, p2New, score1, score2New), n * f, nTimesReached)
-            states.add((not p1Turn, p1, p2New, score1, score2New))
+            addToDict((score1 + score2New, not p1Turn, p1, p2New, score1, score2New), n * f, nTimesReached)
+            states.add((score1 + score2New,not p1Turn, p1, p2New, score1, score2New))
     return states
- 
-statesToConsider = set([(True, pos1, pos2, 0, 0)])
+
+statesToConsider = [(0, True, pos1, pos2, 0, 0)]
+hq.heapify(statesToConsider)
 
 while True:
-    minScoreSumState = (True, pos1, pos2, maxScore, maxScore)
-    for s in statesToConsider:
-        if s[3] + s[4] < minScoreSumState[3] + minScoreSumState[4] and s[3] < maxScore and s[4] < maxScore:
-            minScoreSumState = s
+    print(len(statesToConsider))
+    s = hq.heappop(statesToConsider)
+    while len(statesToConsider) >0 and s == statesToConsider[0]:
+        hq.heappop(statesToConsider)
     
-    if minScoreSumState[3] >= maxScore or minScoreSumState[4] >= maxScore: 
-        print("break")
+    if s[4] >= maxScore or s[5] >= maxScore: 
+        print("Break")
         break
-    statesToConsider.update(sim(*(minScoreSumState + (nTimesReached[minScoreSumState],))))
-    statesToConsider.remove(minScoreSumState)
+    newStates = sim(s[1], s[2], s[3], s[4], s[5], nTimesReached[s])
+    for ns in newStates:
+        hq.heappush(statesToConsider, ns)
     
         
-n1Win = 0
-n2Win = 0
-n1Wins = []
-for p1Turn, p1, p2, score1, score2 in nTimesReached:
+n1Win, n2Win = 0, 0
+for s, p1Turn, p1, p2, score1, score2 in nTimesReached:
     if score1 >= maxScore:
-        assert(score2 < maxScore)
-        n1Win += nTimesReached[(p1Turn, p1, p2, score1, score2)]
-        n1Wins.append(nTimesReached[(p1Turn, p1, p2, score1, score2)])
+        n1Win += nTimesReached[(s, p1Turn, p1, p2, score1, score2)]
     if score2 >= maxScore:
-        assert(score1 < maxScore)
-        n2Win += nTimesReached[(p1Turn, p1, p2, score1, score2)]
-print(n1Win // 2)
-print(n2Win // 2)
+        n2Win += nTimesReached[(s, p1Turn, p1, p2, score1, score2)]
+print(max(n1Win, n2Win))
---
+++ 22.py
@@ -0,0 +1,84 @@
+from dataclasses import dataclass
+lines = [line[:-1] for line in open("22.txt", "r").readlines()]
+
+onOff = []
+xLims = []
+yLims = []
+zLims = []
+
+for line in lines:
+    onOff.append(True if line[1] == "n" else False)
+    toks = line[line.index("x"):].split(",")
+    xLims.append([int(x) for x in toks[0][2:].split("..")])
+    yLims.append([int(y) for y in toks[1][2:].split("..")])
+    zLims.append([int(z) for z in toks[2][2:].split("..")])
+
+@dataclass
+class Box:
+    x1: int
+    x2: int
+    y1: int
+    y2: int
+    z1: int
+    z2: int
+    
+    # def intersects(self, c):
+    #     return (c.x2 > self.x1 or c.x1 > self.x2 and c.y2 > self.y1 or c.y1 > self.y2 and c.z2 > self.z1 or c.z1 > self.z2
+    
+    def contains(self, c):
+        xContains = c.x1 >= self.x1 and c.x2 <= self.x2
+        yContains = c.y1 >= self.y1 and c.y2 <= self.y2
+        zContains = c.z1 >= self.z1 and c.z2 <= self.z2
+        return xContains and yContains and zContains
+    
+    def getIntersectionBox(self, c):
+        xL = max(self.x1, c.x1)
+        xR = min(self.x2, c.x2)
+        yL = max(self.y1, c.y1)
+        yR = min(self.y2, c.y2)
+        zL = max(self.z1, c.z1)
+        zR = min(self.z2, c.z2)
+        return None if xL > xR or yL > yR or zL > zR else Box(xL, xR, yL, yR, zL, zR)
+    
+    def volume(self):
+        return (self.x2 - self.x1 + 1)*(self.y2 - self.y1 + 1)*(self.z2 - self.z1 + 1)
+
+boxes = []
+weights = []
+for i in range(len(onOff)):
+    print("i ", i)
+    newBox = Box(xLims[i][0], xLims[i][1], yLims[i][0], yLims[i][1], zLims[i][0], zLims[i][1])
+    weight = 1 if onOff[i] else -1
+    
+    intersections = []
+    iWeights = []
+    for i, b in enumerate(boxes):
+        intersection = newBox.getIntersectionBox(b)
+        if intersection is not None:
+            intersections.append(intersection)
+            if weight > 0:
+                iWeights.append(-1 if weights[i] > 0 else 1)
+            else:
+                iWeights.append(-1 if weights[i] > 0 else 1)
+            
+    for i, b in enumerate(intersections):
+        if b in boxes:
+            weights[boxes.index(b)] += iWeights[i]
+        else:
+            boxes.append(b)
+            weights.append(iWeights[i])
+    if (weight > 0):
+        if newBox in boxes:
+            weights[boxes.index(newBox)] += weight
+        else:
+            boxes.append(newBox)
+            weights.append(weight)
+    zeroWeightIndices = [i for i in range(len(weights)) if weights[i] == 0]
+    zeroWeightIndices.reverse()
+    for i in zeroWeightIndices:
+        weights.pop(i)
+        boxes.pop(i)
+    
+volume = 0
+for i, b in enumerate(boxes):
+    volume += weights[i] * b.volume()
